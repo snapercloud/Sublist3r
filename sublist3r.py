526b3aaa707bb064d4a72d62ffb57ef581416145
CB <cb@inscale.com.br>
2017-10-09 15:47:45 -0300

    Removing windows support.

--- sublist3r.py
+++ sublist3r.py
@@ -33,29 +33,11 @@ else:
 import requests.packages.urllib3
 
 # Console Colors
-if is_windows:
-    # Windows deserves coloring too :D
-    G = '\033[92m'  # green
-    Y = '\033[93m'  # yellow
-    B = '\033[94m'  # blue
-    R = '\033[91m'  # red
-    W = '\033[0m'   # white
-    try:
-        import win_unicode_console , colorama
-        win_unicode_console.enable()
-        colorama.init()
-        #Now the unicode will work ^_^
-    except:
-        print("[!] Error: Coloring libraries not installed, no coloring will be used [Check the readme]")
-        G = Y = B = R = W = G = Y = B = R = W = ''
-
-
-else:
-    G = '\033[92m'  # green
-    Y = '\033[93m'  # yellow
-    B = '\033[94m'  # blue
-    R = '\033[91m'  # red
-    W = '\033[0m'   # white
+G = '\033[92m'  # green
+Y = '\033[93m'  # yellow
+B = '\033[94m'  # blue
+R = '\033[91m'  # red
+W = '\033[0m'   # white
 
 
 def banner():
@@ -851,10 +833,7 @@ def main(domain, threads, savefile, ports, silent, verbose, enable_bruteforce, e
     bruteforce_list = set()
     search_list = set()
 
-    if is_windows:
-        subdomains_queue = list()
-    else:
-        subdomains_queue = multiprocessing.Manager().list()
+    subdomains_queue = multiprocessing.Manager().list()
 
     # Check Bruteforce Status
     if enable_bruteforce or enable_bruteforce is None:
